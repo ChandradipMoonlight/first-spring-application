fee5db8250f257f03018c0a15afc749abad97185
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-08-07 16:57:09 +0530

    Created: springApp to display hello form bridgelabz

--- src/main/java/com/firstspringapp/FirstSpringAppApplication.java
+++ src/main/java/com/firstspringapp/FirstSpringAppApplication.java
@@ -3,12 +3,17 @@ package com.firstspringapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * @SpringBootApplication this annotation enables Spring Boot's Auto
+ * configuration mechanism.
+ * same as @Configuration @EnableAutoConfiguration @ComponentScan
+ */
 @SpringBootApplication
 public class FirstSpringAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FirstSpringAppApplication.class, args);
+        System.out.println("Hello World from BridgeLabz");
     }
 
 }
