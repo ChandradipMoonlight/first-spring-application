427408ccc802166f67176f3a5732bc4f049d112b
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-08-08 13:19:01 +0530

    Created: spring project to understand the spring concept

--- src/main/java/com/firstspringapp/HelloWorldApplication.java
+++ src/main/java/com/firstspringapp/HelloWorldApplication.java
@@ -1,7 +1,10 @@
 package com.firstspringapp;
 
+import com.firstspringapp.component.DemoBean;
+import com.firstspringapp.controller.HelloRestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 /**
  * @SpringBootApplication this annotation enables Spring Boot's Auto
@@ -12,8 +15,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
         System.out.println("Hello World from BridgeLabz");
+        ApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args);
+        DemoBean demoBean = context.getBean(DemoBean.class);
+        System.out.println("Demo Bean = "+ demoBean);
+        System.out.println(context.getBean(HelloRestController.class));
     }
 
 }
